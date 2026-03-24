164b88cdc954a725c5d54bfcb3b9b9a727a505f1
Michael Zeller <cc231050@fhstp.ac.at>
2026-03-24 21:31:14 +0100

    Basic Video Viewer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let src: string | null = $state(null);
+	let currentTime: number = $state(0);
+</script>
+
+<input 
+	type="file" 
+	accept="video/*" 
+	onchange={(e) => {
+		const input = e.currentTarget as HTMLInputElement;
+		const file = input.files?.[0];
+		if (file) {
+			if (src) URL.revokeObjectURL(src);
+			src = URL.createObjectURL(file);
+			console.log('src set to', src);
+		}
+	}} 
+/>
+
+{#if src}
+  <!-- svelte-ignore a11y-media-has-caption -->
+  <video {src} bind:currentTime controls preload="metadata" />
+  <p>
+    Time: {currentTime}
+  </p>
+{/if}
+
+<style>
+	video {
+		display: block;
+		width: 100%;
+		max-width: 800px;
+		margin: 1rem 0;
+	}
+</style>
